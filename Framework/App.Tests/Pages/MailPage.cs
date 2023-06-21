80d0c7500537d7c0e4cc0bb7acc28e17d6f00041
Sirojbek Azimjonov <joris5969@gmail.com>
2023-06-21 19:26:19 +0500

    Framework Initial

---
+++ Framework/App.Tests/Pages/MailPage.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using static System.Net.WebRequestMethods;
+
+namespace App.Tests.Pages
+{
+	public class MailPage
+	{
+		private readonly IWebDriver _driver;
+		private readonly WebDriverWait _wait;
+
+		private const string _mailServer = "https://yopmail.com/";
+		private const string _email = "sampleapp@yopmail.com";
+
+
+		public MailPage(IWebDriver driver, WebDriverWait wait)
+        {
+			_driver = driver;
+			_wait = wait;
+			PageFactory.InitElements(_driver, this);
+		}
+
+		[FindsBy(How = How.XPath, Using = "//input[@id='login']")]
+		public IWebElement EmailInput { get; set; }
+
+		public void EnterToMail()
+		{
+			_driver.SwitchTo().NewWindow(WindowType.Tab);
+			_driver.Navigate().GoToUrl(_mailServer);
+			EmailInput.SendKeys(_email + Keys.Enter);
+		}
+    }
+}
---
+++ Framework/App.Tests/Pages/SearchPage.cs
@@ -0,0 +1,44 @@
+﻿using SeleniumExtras.PageObjects;
+
+namespace App.Tests.Pages
+{
+    public class SearchPage
+    {
+        private readonly IWebDriver _driver;
+        private readonly WebDriverWait _wait;
+
+        private readonly string _url = "https://cloud.google.com/ ";
+        private readonly string _numberOfInstances = "4";
+
+        public SearchPage(IWebDriver driver, WebDriverWait wait)
+        {
+            _driver = driver;
+            _wait = wait;
+            PageFactory.InitElements(_driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//input[@name='q']")]
+        public IWebElement SearchBox { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "(//a[@class='gs-title']/b[text()='Google Cloud Pricing Calculator'])[1]")]
+        public IWebElement SearchResult { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//input[@id='input_95']")]
+        public IWebElement NumberOfInstancesInput { get; set; }
+
+        public void Navigate()
+        {
+            _driver.Navigate().GoToUrl(_url);
+        }
+
+        public void Search(string textToType)
+        {
+            _wait.Until(ExpectedConditions.ElementToBeClickable(SearchBox));
+            SearchBox.Clear();
+            SearchBox.SendKeys(textToType + Keys.Enter);
+            _wait.Until(ExpectedConditions.ElementToBeClickable(SearchResult)).Click();
+
+        }
+
+    }
+}
